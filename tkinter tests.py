4aa663507693090fbbc667b739a1a032b1782858
Ko Sakuma <64699157+ko-sakuma@users.noreply.github.com>
2021-01-26 08:53:38 +0000

    Delete tkinter tests.py

--- tkinter tests.py
+++
@@ -1,7 +0,0 @@
-columns = ("hello", "there", "skywalker")
-columns_to_update = ""
-for x in columns:
-    columns_to_update += ', ' + ('%s = ?' % x)
-columns_to_update = columns_to_update[2:]
-
-print(columns_to_update)
